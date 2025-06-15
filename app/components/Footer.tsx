4bb601f9bcc942284095d0bd5078e59f79d6bc2e
Ali-Rahimpoor <ali.rahimpoor.ar@gmail.com>
2025-06-15 12:26:52 +0330

    add Footer

---
+++ app/components/Footer.tsx
@@ -0,0 +1,11 @@
+import { FaGithub } from "react-icons/fa6";
+const Footer = ()=>{
+
+   return(
+      <footer className="flex items-center justify-between font-Karla px-10">
+         <h1>© 2025 Alex Developer. All rights reserved.</h1>
+         <FaGithub size={30} />
+      </footer>
+   )
+}
+export default Footer;
